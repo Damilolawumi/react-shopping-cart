bbc69db3ae64b429197650e6c9cfcb838b4a847c
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-07 18:05:32 +0100

    refractored our products route to no longer use render props.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from '../src/contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -17,16 +18,12 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
-			{/* Routes */}
+			
 			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+				exact path="/" component={Products}
 					/>
 				)}
 			/>
@@ -35,6 +32,7 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
