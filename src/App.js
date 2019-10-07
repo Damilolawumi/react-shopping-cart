bfd856e9585fe6d2ea3a00a838db164c9782aa7b
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-07 19:30:06 +0100

    deleted some mistakes in my app.js

--- src/App.js
+++ src/App.js
@@ -20,20 +20,19 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-			<CartContext.Provider value={cart}>
-			<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-			
-			<Route
-				exact path="/" component={Products}
+
+					<Route
+						exact path="/" component={Products}
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart" component={cart} />}
-			/>
-			</CartContext.Provider>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
